a3815a322a15aa712d1d86fccf6ea9dd0eb4b9c3
M.R.K.W. DE SILVA <rumindukavishka14@gmail.com>
2024-10-03 11:42:31 +0530

    updating Object - Example of an incorrect update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
-
 const App = () => {
   const [drink, setDrink] = useState({ title: "Coke", price: 5 });
-  const handleClick = () => {};
+  const handleClick = () => {
+    drink.price = 6;
+    // This won't work because React doesn’t detect the change.
+    setDrink(drink);
+  };
   return (
     <div>
+      {/* to display price update */}
+      {drink.price}
       <button onClick={handleClick}>Click me</button>
     </div>
   );

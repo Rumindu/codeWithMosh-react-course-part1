185df2ed79c5e90c6edac8ce38b1d06e5700feef
M.R.K.W. DE SILVA <rumindukavishka14@gmail.com>
2024-10-06 10:09:14 +0530

    Updating nested object

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,19 @@ const App = () => {
     name: "John Doe",
     address: { city: "New York", zipCode: "10001" },
   });
-  const handleClick = () => {};
+  const handleClick = () => {
+    setCustomer({
+      ...customer, // Spread the existing customer object
+      address: {
+        // Create a new address object
+        ...customer.address, // Spread the existing address object
+        zipCode: "94112", // Update the zipCode
+      },
+    });
+  };
   return (
     <div>
+      {customer.address.zipCode}
       <button onClick={handleClick}>Click me</button>
     </div>
   );

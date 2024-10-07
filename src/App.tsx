1766db7552ed317f318f67faeba2eb453416ddcd
M.R.K.W. DE SILVA <rumindukavishka14@gmail.com>
2024-10-07 05:50:37 +0530

    Updating State Arrays in React

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [tags, setTags] = useState(["happy", "sad", "excited"]);
-  const handleClick = () => {};
+  const handleClick = () => {
+    // Final setTag() method only execute. please comment others to see relevant output
+
+    //Add new item to the array
+    setTags([...tags, "cheerful"]);
+
+    // Remove item from the array
+    setTags(tags.filter((tag) => tag !== "sad"));
+
+    // Update item in the array
+    setTags(tags.map((tag) => (tag === "excited" ? "thrilled" : tag)));
+  };
   return (
     <div>
       {tags.join(", ")}

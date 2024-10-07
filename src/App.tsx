5d2d6dc78ee3efae0b3e753a36194a27aa9ee42b
M.R.K.W. DE SILVA <rumindukavishka14@gmail.com>
2024-10-07 05:50:12 +0530

    Updating State Arrays in React - Initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import { useState } from "react";
 
 const App = () => {
-  const [customer, setCustomer] = useState({
-    name: "John Doe",
-    address: { city: "New York", zipCode: "10001" },
-  });
-  const handleClick = () => {
-    setCustomer({
-      ...customer, // Spread the existing customer object
-      address: {
-        // Create a new address object
-        ...customer.address, // Spread the existing address object
-        zipCode: "94112", // Update the zipCode
-      },
-    });
-  };
+  const [tags, setTags] = useState(["happy", "sad", "excited"]);
+  const handleClick = () => {};
   return (
     <div>
-      {customer.address.zipCode}
-      <button onClick={handleClick}>Click me</button>
+      {tags.join(", ")}
+      <button style={{ marginLeft: "5px" }} onClick={handleClick}>
+        Click me
+      </button>
     </div>
   );
 };
